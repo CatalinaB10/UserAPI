a3f665f3537dc3c9b0302d50d94d4e31525ea770
catalina.buzan <catalina.buzan@LAPTOPTRK110>
2024-10-09 10:43:14 +0300

    Add project files.

---
+++ UserAPI/Models/User.cs
@@ -0,0 +1,9 @@
+﻿namespace UserAPI.Models
+{
+    public class User
+    {
+        public long Id { get; set; }
+        public string Name { get; set; }
+        public string Role { get; set; }
+    }
+}
---
+++ UserAPI/Models/UserDTO.cs
@@ -0,0 +1,9 @@
+﻿namespace UserAPI.Models
+{
+    public class UserDTO
+    {
+        public long Id { get; set; }
+        public string Name { get; set; }
+        public string Role { get; set; }
+    }
+}
